f2db287fd07a666dde0bba2932337c44966118c2
dldydgns530@gmail.com <dldydgns530@gmail.com>
2022-06-11 21:33:53 +0900

    [init]: backend setting

---
+++ backend/src/main/java/com/capstone/drugmanagementapp/DrugmanagementappApplication.java
@@ -0,0 +1,13 @@
+package com.capstone.drugmanagementapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DrugmanagementappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DrugmanagementappApplication.class, args);
+	}
+
+}
