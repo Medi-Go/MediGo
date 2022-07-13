ac23559d0c5445dee03613a7bb660d0ed9c64d68
Jin1won <wlsdnjs16521@gmail.com>
2022-07-14 02:28:16 +0900

    [chore]: 백엔드 merge

---
+++ backend/src/main/java/com/capstone/medigo/api/TokenController.java
@@ -0,0 +1,44 @@
+package com.capstone.medigo.api;
+
+import static com.capstone.medigo.domain.Role.ROLE_USER;
+import static org.springframework.http.HttpHeaders.AUTHORIZATION;
+
+import com.capstone.medigo.global.token.Token;
+import com.capstone.medigo.global.token.TokenService;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/token")
+public class TokenController {
+    private final TokenService tokenService;
+
+    @GetMapping("/expired")
+    public String auth() {
+        throw new RuntimeException();
+    }
+
+    @PostMapping("/refresh")
+    public String refreshAuth(HttpServletRequest request, HttpServletResponse response) {
+        String refreshToken = request.getHeader("Refresh");
+
+        if (refreshToken != null && tokenService.verifyToken(refreshToken)) {
+            String email = tokenService.getUid(refreshToken);
+            Token newToken = tokenService.generateToken(email, ROLE_USER.stringValue);
+
+            response.addHeader(AUTHORIZATION, newToken.getAccessToken());
+            response.addHeader("Refresh", newToken.getRefreshToken());
+            response.setContentType("application/json;charset=UTF-8");
+
+            return "HAPPY NEW TOKEN";
+        }
+
+        throw new RuntimeException();
+    }
+}
