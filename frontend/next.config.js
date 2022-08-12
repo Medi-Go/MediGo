3d34da2301da42e1b2505e0df02bf7c67a2bf6d6
Jin1won <wlsdnjs16521@gmail.com>
2022-08-12 17:10:07 +0900

    [refactor]: pwa & module transpiler 관련 설정

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,9 +1,32 @@
 /** @type {import('next').NextConfig} */
 
+const withPlugins = require('next-compose-plugins');
+
+const withPWA = require('next-pwa');
+
 const withTM = require('next-transpile-modules')([
   '@fullcalendar/common',
+  '@babel/preset-react',
+  '@fullcalendar/common',
   '@fullcalendar/daygrid',
+  '@fullcalendar/interaction',
   '@fullcalendar/react',
+  '@fullcalendar/timegrid',
 ]);
 
-module.exports = withTM({});
+module.exports = withPlugins([
+  [withPWA, withTM],
+  {
+    reactStrictMode: true,
+    swcMinify: true,
+    pwa: {
+      dest: 'public',
+      register: true,
+      skipWaiting: true,
+      disable: process.env.NODE_ENV === 'development',
+    },
+    compiler: {
+      styledComponents: true,
+    },
+  },
+]);
